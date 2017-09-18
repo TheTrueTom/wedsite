580560744637b62129b2633c3eee9b33759fdf47
Thomas Brichart <thomas@brichart.fr>
2017-09-18 19:01:50 +0200

    Add name to the music proposal and save results in a csv instead of sending mails

--- src/song-form-handler.php
+++ src/song-form-handler.php
@@ -1,20 +1,23 @@
 <?php 
 $errors = '';
 $myemail = 'mariage@delphinethomas.me';
+$file = 'propositions.csv';
 
-if(empty($_POST['name']))
+if(empty($_POST['name'])  || 
+   empty($_POST['musique']))
 {
     $errors .= "\n Error: all fields are required";
 }
 
 $name = $_POST['name'];
+$musique = $_POST['musique'];
 $ip = $_SERVER['REMOTE_ADDR'];
 
-if( empty($errors))
+/*if(empty($errors))
 {
 	$to = $myemail; 
 	$email_subject = "Musique proposée";
-	$email_body = "Musique : $name"; 
+	$email_body = "Musique proposée par $name: $musique"; 
 	
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $myemail";
@@ -23,7 +26,17 @@ if( empty($errors))
 
 	//redirect to the 'thank you' page
 	header("Location: ../index.html?success=2");
-} 
+} */
+
+if (empty($errors))
+{
+	$current = file_get_contents($file);
+	$current .= "$name; $musique;\n";
+	file_put_contents($file, $current);
+
+	header("Location: ../index.html?success=2&name=$name");
+}
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
